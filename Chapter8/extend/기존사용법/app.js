e566631eb4ecc0cf429a0c25baf6531a98c26a1a
jamie han <jamie@snowkiwi.com>
2017-05-24 18:28:53 +0900

    commit

---
+++ Chapter8/extend/기존사용법/app.js
@@ -0,0 +1,5 @@
+// 구형 node에서 에러
+
+import {foo, bar} from 'foobar';
+console.log(foo());
+console.log(bar());
---
+++ Chapter8/extend/기존사용법/foobar.js
@@ -0,0 +1,6 @@
+export function foo() {
+  return 'bar';
+}
+export function bar() {
+  return 'foo';
+}
